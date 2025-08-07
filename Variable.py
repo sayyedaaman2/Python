9d5cd5dd0de239623394e13e6f4ce1c7acc69691
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-07 21:00:25 +0530

    variable

---
+++ Variable.py
@@ -0,0 +1,51 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#           A variable behaves as if it was the value it contains
+
+
+
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "pizzabro123@gmail.com"
+
+# print(f"Hello {first_name}")
+# print(f"I like to eat a {food}")
+# print(f"This is my email address : {email}")
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+# print(f"You are {age} year old.")
+# print(f"You have {quantity} pens.")
+# print(f"class has {num_of_students} students.")
+
+# Float 
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# print(f"The price is  {price}.")
+# print(f"Your gpa is : {gpa}")
+# print(f"You ran {distance} km.")
+
+# Boolean
+
+is_student = True
+is_admin = False
+for_sale = True
+is_online = True
+# print(f"Are you a student? : {is_student}")
+# print(f"Is it a admin : {is_admin}")
+
+# if is_student:
+#     print("You are a student.")
+# else: 
+#     print("You are not a student.")
+
+# if is_admin:
+#     print("You are a admin.")
+# else:
+#     print("You are not a admin.")
+
