d875e61b275bc85f1c36b1bed3b59ac63a95d5d0
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-07 21:52:14 +0530

    Math Module

---
+++ MathModule.py
@@ -0,0 +1,39 @@
+import  math
+
+x = 9.5
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)
+# result = math.floor(x)
+# print(result)
+
+
+#Exercise 1 : calculate circle radius
+
+# radius = float(input("Enter the radius of a circle : "))
+
+# circumference = 2 * math.pi * radius
+
+# print(f"The circumference is {round(circumference,2)}cm")
+
+
+#Exercise 2 : calculate area of circle
+
+# radius = float(input("Enter the radius of a circle : "))
+
+# area = math.pi * pow(radius,2)
+
+# print(f"The area of the circle is {round(area,2)}cm^2")
+
+
+#Exercise 3 : calculate right triangle
+
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+c = math.sqrt(pow(a,2) + pow(b,2))
+
+print(f"Side C = {c}")
+
