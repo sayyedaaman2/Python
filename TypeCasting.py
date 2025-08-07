a831e9ae84f04776afc37c650bb8e17ad98c3e8b
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-07 21:26:24 +0530

    type casting

---
+++ TypeCasting.py
@@ -0,0 +1,29 @@
+# Typecasting = the process of converting a variable from one data type to another 
+#               str(), int(), float(), bool()
+
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_student))
+
+
+gpa = int(gpa); # Convert into integer value
+# print(gpa)
+# print(type(gpa))
+
+age = float(age) # convert into float value
+# print(age)
+# print(type(age))
+
+
+age = str(age) # convert into string value
+# print(type(age))
+
+name = bool(name) # convert into boolean value
+print(name)
+print(type(name))
