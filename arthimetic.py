2624618334e6e7837918305a8a859c324f397b21
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-07 21:51:56 +0530

    Arthimetic Operators

---
+++ arthimetic.py
@@ -0,0 +1,34 @@
+
+friends = 10
+
+# friends = friends + 1
+# friends += 1
+
+# friends = friends -2 
+# friends -= 2
+
+# friends = friends * 3
+# friends *= 3
+
+# friends  = friends / 2
+# friends /= 2
+
+# friends = friends ** 2 
+# friends **= 2
+
+# remainder = friends % 2
+
+# print(friends)
+
+# Math functions
+
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = max(x,y,z);
+# result = min (x,y,z)
+# print(result)
