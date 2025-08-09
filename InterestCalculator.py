512386dd988dfe23dba3fe7d87a9e9898cc4ba0f
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-09 21:52:45 +0530

    while loop and compond interest

---
+++ InterestCalculator.py
@@ -0,0 +1,29 @@
+# Python compond interest calculator
+
+principle = 0
+rate = 0
+time = 0
+
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle < 0:
+        print("Principle can't be less than zero")
+    else : 
+        break
+
+while True:
+    rate = float(input("Enter the interest rate : "))
+    if rate < 0:
+        print("Rate can't be less than zero")
+    else : 
+        break
+while True:
+    time = int(input("Enter the time in years : "))
+    if time < 0:
+        print("time can't be less than zero")
+    else:
+        break
+
+total = principle * pow(1 + rate / 100, time)
+
+print(f"Balance after {time} year/s: ${total:.2f}")
