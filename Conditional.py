41c526b274d941532eebcd43fbbc659785be0c20
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-09 20:17:50 +0530

    Conditional 'if elif else'

---
+++ Conditional.py
@@ -0,0 +1,39 @@
+# if = Do some code only IF some condition is True
+#       Else do something else
+
+# age = int(input("Enter your age: "))
+# if age >= 100:
+#     print("You are to old to sign up")
+# elif age >= 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't been born yet!")
+# elif age >= 100:
+#     print("You are to old to sign up")
+# else:
+#     print("You must be 18+ to sign up")
+
+
+# response = input("Would you like food (Y/N):")
+
+# if response == "Y":
+#     print("Have some food!")
+# else:
+#     print("No food for you!")
+
+
+# name = input("Enter your name: ")
+
+# if name == "":
+#     print("you did not type in your name!")
+# else:
+#     print(f"Hello {name}")
+
+
+# for_sale = True
+
+# if for_sale:
+#     print("This item is for sale")
+# else:
+#     print("This item is NOT for sale")
+
