90619d00ab66f0608c5293479d7a2ce9d2fc7bb6
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-09 20:42:55 +0530

    String index

---
+++ StringIndex.py
@@ -0,0 +1,19 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#           [start: end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[0])
+
+# print(credit_number[:4])
+
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-1])
+# print(credit_number[::3])
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# credit_number = credit_number[::-1] # Reverse the string
+# print(credit_number)
