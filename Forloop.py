a8a4a711a66b7564a3e784a5f2beab3437d0d18f
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-09 22:06:39 +0530

    For loops

---
+++ Forloop.py
@@ -0,0 +1,40 @@
+# for loops = execute a blog of code a fixed number of times.
+#             You can iterate over a range, string, sequence etc.
+
+
+# for x in range(1,11):
+#     print(x)
+
+
+
+# for x in reversed(range(1,11)):
+#     print(x)    
+
+# print(" HAPPY NEW YEAR!")
+
+
+
+# for x in range(1,11,3):
+#     print(x)    
+
+# print(" HAPPY NEW YEAR!")
+
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card:
+#     print(x)
+
+# for x in range(1,21):
+#     if x == 13:
+#         continue
+#     else:
+#         print(x)
+
+# for x in range(1,21):
+#     if x == 13:
+#         break
+#     else:
+#         print(x)        
+
+
