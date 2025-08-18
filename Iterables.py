7d9c4e3b0b69f0eabe247f99f3fc78199f1f276e
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-08-18 21:19:10 +0530

    Iterables

---
+++ Iterables.py
@@ -0,0 +1,58 @@
+# Iterables = An object/collection that can return its elements one at a time,
+#               allowing to to be itereated over in a loop
+
+
+# List
+
+# numbers = [1,2,3,4,5]
+
+
+# for num in (numbers):
+#     print(num, end=" ")
+    
+# for num in reversed(numbers):
+#     print(num, end=" ")
+
+# Tuple
+
+# numbers = (1,2,3,4,5)
+
+
+# for num in (numbers):
+#     print(num, end=" ")
+    
+# print()
+# for num in reversed(numbers):
+#     print(num, end=" ")
+
+# Sets
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+# # set object is not reversible
+# for fruit in reversed(fruits):
+#     print(fruit)
+
+
+# String
+
+# name = "Bro Code"
+
+# for character in name:
+#     print(character,end=" ")
+
+# Dictionary 
+
+# my_dictionary = {"A": 1, "B":2, "C":3}
+
+# for key in my_dictionary:
+#     print(f"key : {key}")
+
+# for value in my_dictionary.values():
+#     print(f"value : {value}")
+
+# for key,value in my_dictionary.items():
+#     print(f"key:{key},value:{value}")
